c2179e8fef16de07fde08f4544af0f9eb6f31dbb
Manikkumar K <maniksrr@gmail.com>
2019-10-08 08:38:11 +0530

    adding sudo

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -4,9 +4,9 @@ set -eu
 
 git remote add heroku https://git.heroku.com/$HEROKU_APP_NAME_ONE.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
-mkdir -p /usr/local/lib /usr/local/bin
-tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
@@ -17,7 +17,7 @@ machine git.heroku.com
 	password $HEROKU_API_KEY
 EOF
 
-chmod 600 ~/.netrc
+sudo chmod 600 ~/.netrc
 
 # Add heroku.com to the list of known hosts
-ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
