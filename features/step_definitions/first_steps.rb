9e1cd6d93d1a0feb8a6d843725d2b0e59b067fc7
Manikkumar K <maniksrr@gmail.com>
2019-10-08 13:52:20 +0530

    diff approach on functional testing

--- features/step_definitions/first_steps.rb
+++ features/step_definitions/first_steps.rb
@@ -4,12 +4,12 @@ require 'rest-client'
 
 
 When("I hit the API") do
-  response = RestClient.get 'http://localhost:4567/hello'
+  response = RestClient.get 'http://idonethis-backend/hello'
   expect(response.code).to eq(200)
 end
 
 Then("hello world should return") do
-  response = RestClient.get 'http://localhost:4567/hello'
+  response = RestClient.get 'http://idonethis-backend/hello'
   expect(response.code).to eq(200)
   data = response.body
 end
