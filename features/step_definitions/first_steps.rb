dcaa224248c4798b4aef31e5bd074affb88b9a1e
Manikkumar K <maniksrr@gmail.com>
2019-10-08 13:56:45 +0530

    directly hitting deployed url

--- features/step_definitions/first_steps.rb
+++ features/step_definitions/first_steps.rb
@@ -4,12 +4,12 @@ require 'rest-client'
 
 
 When("I hit the API") do
-  response = RestClient.get 'http://idonethis-backend/hello'
+  response = RestClient.get 'https://idonethis-backend.herokuapp.com/hello'
   expect(response.code).to eq(200)
 end
 
 Then("hello world should return") do
-  response = RestClient.get 'http://idonethis-backend/hello'
+  response = RestClient.get 'https://idonethis-backend.herokuapp.com/hello'
   expect(response.code).to eq(200)
   data = response.body
 end
