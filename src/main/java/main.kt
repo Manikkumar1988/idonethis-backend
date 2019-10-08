9eedf85b7c4375357b8352df9b077ec3efda63c2
Manikkumar K <maniksrr@gmail.com>
2019-10-08 08:10:05 +0530

    Heroku deploy

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -3,5 +3,13 @@ import model.Model
 import spark.Spark.*
 
 fun main() {
+    port(getHerokuAssignedPort())
     get("/hello", GetHandler(Model()))
+}
+
+fun getHerokuAssignedPort(): Int {
+    val processBuilder = ProcessBuilder()
+    return if (processBuilder.environment()["PORT"] != null) {
+        Integer.parseInt(processBuilder.environment()["PORT"])
+    } else 4567
 }
