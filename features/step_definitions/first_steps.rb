5debfe609ae15672cf7a63889b37c1b3e79176f1
Manikkumar K <maniksrr@gmail.com>
2019-10-23 16:43:55 +0530

    functional testing loads url from environment

--- features/step_definitions/first_steps.rb
+++ features/step_definitions/first_steps.rb
@@ -4,12 +4,12 @@ require 'rest-client'
 
 
 When("I hit the API") do
-  response = RestClient.get 'https://idonethis-backend.herokuapp.com/hello'
+  response = RestClient.get "#{$BASE_URL}/hello"
   expect(response.code).to eq(200)
 end
 
 Then("hello world should return") do
-  response = RestClient.get 'https://idonethis-backend.herokuapp.com/hello'
+  response = RestClient.get "#{$BASE_URL}/hello"
   expect(response.code).to eq(200)
   data = response.body
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,5 @@
 require 'rspec/expectations'
 World(RSpec::Matchers)
+
+ENV['TEST_ENV'] ||= 'http://localhost:4567'
+$BASE_URL = ENV['TEST_ENV']
