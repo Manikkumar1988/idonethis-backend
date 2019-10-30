d0044db667e7fcb67aa6d58a5d6892b07d0faf38
Manikkumar K <maniksrr@gmail.com>
2019-10-30 21:49:04 +0530

    removed the hard-coded value

--- src/main/java/handler/TeamPostHandler.kt
+++ src/main/java/handler/TeamPostHandler.kt
@@ -7,7 +7,7 @@ import model.Model
 
 class TeamPostHandler(valueClass: Model) : AbstractRequestHandler<Team>(Team::class.java, valueClass) {
     override fun processImpl(value: Team, urlParams: Map<String, String>): Answer {
-        model.addTeamMember("1", value)
+        model.addTeamMember(urlParams.getOrDefault(":teamId","1"), value)
 
         val gson = Gson()
         val jsonString = gson.toJson(Status("success"))
